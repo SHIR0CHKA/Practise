0725319d4381305794d3842e995391e33bb17ff7
Shirochka <plakhty2017@gmail.com>
2023-02-26 23:49:46 +0300

    Конкретизация

--- Practise/Program.cs
+++ Practise/Program.cs
@@ -1,8 +1,9 @@
-﻿for (int i = 1; i < 10; i++)
+﻿for (int i = 1; i < 11; i++)
 {
-    for (int j = 1; j < 10; j++)
+    for (int j = 1; j < 11; j++)
     {
         Console.Write($"{i * j} \t");
     }
     Console.WriteLine();
 }
+//Таблица умножения
